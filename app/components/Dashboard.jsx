6cfaedaedcad262554f1a418650382c8a9d7386d
Subhradip D <virtuallified@gmail.com>
2023-09-06 02:13:36 +0530

    added navbar and ui improvement

--- app/components/Dashboard.jsx
+++ app/components/Dashboard.jsx
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Card, Container, Flex, Switch, Text, Button } from "@radix-ui/themes";
+import {
+  Card,
+  Container,
+  Flex,
+  Switch,
+  Text,
+  Button,
+  Box,
+  Inset,
+} from "@radix-ui/themes";
+import { DashboardIcon } from "@radix-ui/react-icons";
 import { getRealTimeSensorData, setTurnOnOff } from "../api/realtime/route";
 import { useAuthState } from "@/app/utils/authUtils";
 
@@ -45,28 +55,64 @@ const Dashboard = () => {
   };
 
   return (
-    <div>
-      <h2>Dashboard</h2>
-      <div id="dashboard">
-        {data && (
-          <Container key={data.device_id}>
+    <>
+      <Box
+        style={{
+          background: "var(--gray-a2)",
+          borderRadius: "var(--radius-3)",
+        }}>
+        <br />
+        <Container id="dashboard" size="3">
+          <Card size="3">
             <Flex>
-              <Card>
-                <Text>Humidity: {data.humidity}</Text>
-                <Text>Temperature: {data.temperature}</Text>
-                <p>Live Status: {data.liveStatus}</p>
-                <Switch
-                  checked={switchState}
-                  onClick={() =>
-                    handleLiveStatusToggle(data.device_id, data.liveStatus)
-                  }></Switch>
-              </Card>
+              <Inset side="left" mr="5">
+                <Flex
+                  align="center"
+                  justify="center"
+                  px="7"
+                  style={{ background: "#24292F", height: "100%" }}>
+                  <DashboardIcon color="white" height="40" width="40" />
+                </Flex>
+              </Inset>
+              <Box>
+                <Text as="div" color="gray" mb="1" size="2">
+                  <h2>Dashboard</h2>
+                </Text>
+                <Text>
+                  {data && (
+                    <Flex gap="3" key={data.device_id}>
+                      <Card size="3" style={{ width: 200 }}>
+                        <p>Humidity: {data.humidity}</p>
+                        <p>Temperature: {data.temperature}</p>
+                      </Card>
+                      <Card style={{ width: 200 }}>
+                        <p>Device ID: {data.device_id}</p>
+                        <p>Device Name: {data.device_name}</p>
+                        <p>Live Status: {data.liveStatus ? "On" : "Off"}</p>
+                      </Card>
+                      <Card
+                        size="3"
+                        style={{ width: 200, textAlign: "center" }}>
+                        <p>Turn On/Off:</p>
+                        <Switch
+                          checked={switchState}
+                          onClick={() =>
+                            handleLiveStatusToggle(
+                              data.device_id,
+                              data.liveStatus
+                            )
+                          }></Switch>
+                      </Card>
+                    </Flex>
+                  )}
+                </Text>
+              </Box>
             </Flex>
-          </Container>
-        )}
-      </div>
-      <Button onClick={signOut}>Logout</Button>
-    </div>
+          </Card>
+          <Button onClick={signOut}>Logout</Button>
+        </Container>
+      </Box>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,81 @@
+const plugin = require("tailwindcss/plugin");
+const { blackA, mauve, violet, indigo, purple } = require("@radix-ui/colors");
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        ...blackA,
+        ...mauve,
+        ...violet,
+        ...purple,
+        ...indigo,
+      },
+      keyframes: {
+        enterFromRight: {
+          from: { opacity: 0, transform: "translateX(200px)" },
+          to: { opacity: 1, transform: "translateX(0)" },
+        },
+        enterFromLeft: {
+          from: { opacity: 0, transform: "translateX(-200px)" },
+          to: { opacity: 1, transform: "translateX(0)" },
+        },
+        exitToRight: {
+          from: { opacity: 1, transform: "translateX(0)" },
+          to: { opacity: 0, transform: "translateX(200px)" },
+        },
+        exitToLeft: {
+          from: { opacity: 1, transform: "translateX(0)" },
+          to: { opacity: 0, transform: "translateX(-200px)" },
+        },
+        scaleIn: {
+          from: { opacity: 0, transform: "rotateX(-10deg) scale(0.9)" },
+          to: { opacity: 1, transform: "rotateX(0deg) scale(1)" },
+        },
+        scaleOut: {
+          from: { opacity: 1, transform: "rotateX(0deg) scale(1)" },
+          to: { opacity: 0, transform: "rotateX(-10deg) scale(0.95)" },
+        },
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        fadeOut: {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
+        },
       },
     },
+    animation: {
+      scaleIn: "scaleIn 200ms ease",
+      scaleOut: "scaleOut 200ms ease",
+      fadeIn: "fadeIn 200ms ease",
+      fadeOut: "fadeOut 200ms ease",
+      enterFromLeft: "enterFromLeft 250ms ease",
+      enterFromRight: "enterFromRight 250ms ease",
+      exitToLeft: "exitToLeft 250ms ease",
+      exitToRight: "exitToRight 250ms ease",
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(({ matchUtilities }) => {
+      matchUtilities({
+        perspective: (value) => ({
+          perspective: value,
+        }),
+      });
+    }),
+  ],
+};
