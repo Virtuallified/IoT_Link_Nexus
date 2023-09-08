5627cffcfd6ba2e5ee502e6796540a2829894dfb
Subhradip D <virtuallified@gmail.com>
2023-09-09 01:18:01 +0530

    dark snippet added

--- Client/app/layout.jsx
+++ Client/app/layout.jsx
@@ -13,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* <body className={`${inter.className} dark text-foreground bg-background`}> */}
       <body className={inter.className}>
         <Providers>
           <ThemeProvider>{children}</ThemeProvider>
