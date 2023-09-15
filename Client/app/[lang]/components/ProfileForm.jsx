6a748cd4669b2dab7197248dc96c0d718e8464e6
Subhradip D <virtuallified@gmail.com>
2023-09-16 03:04:02 +0530

    initial values for profile pages from store

--- Client/app/[lang]/components/ProfileForm.jsx
+++ Client/app/[lang]/components/ProfileForm.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Field, reduxForm } from "redux-form";
+import { Field, reduxForm, initialize } from "redux-form";
 import { BlurBottom } from "./reusable/BlurBack";
 
-const ProfileForm = ({ handleSubmit }) => {
+const ProfileForm = ({ handleSubmit, dispatch }) => {
   const user = useSelector((state) => state.user);
+
+  React.useEffect(() => {
+    // Initialize the form with the initial value
+    dispatch(
+      initialize("profile", {
+        displayName: user.displayName,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        photoURL: user.photoURL,
+      })
+    );
+  }, [dispatch, user]);
 
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-20 lg:mx-auto lg:px-8">
@@ -21,7 +33,7 @@ const ProfileForm = ({ handleSubmit }) => {
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
               <label
-                htmlFor="first-name"
+                htmlFor="name"
                 className="block text-sm font-medium leading-6 text-gray-900">
                 Name
               </label>
@@ -29,10 +41,30 @@ const ProfileForm = ({ handleSubmit }) => {
                 <Field
                   component="input"
                   type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  name="name"
+                  id="name"
+                  autoComplete="name"
+                  placeholder={!user.displayName && "Test User"}
+                  className="px-4 py-2 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="phone"
+                className="block text-sm font-medium leading-6 text-gray-900">
+                Mobile
+              </label>
+              <div className="mt-2">
+                <Field
+                  component="input"
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  autoComplete="phone"
+                  placeholder={!user.phoneNumber && "98xxxxxx37"}
+                  className="px-4 py-2 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -50,31 +82,9 @@ const ProfileForm = ({ handleSubmit }) => {
                   name="email"
                   type="email"
                   autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder={!user.email && "example@iotlinknexus.com"}
+                  className="px-4 py-2 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
-              </div>
-            </div>
-
-            <div className="sm:col-span-3">
-              <label
-                htmlFor="username"
-                className="block text-sm font-medium leading-6 text-gray-900">
-                Username
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
-                    iotlinknexus.com/
-                  </span>
-                  <input
-                    type="text"
-                    name="username"
-                    id="username"
-                    autoComplete="username"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    placeholder="janesmith"
-                  />
-                </div>
               </div>
             </div>
 
